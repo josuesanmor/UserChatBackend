2bd47de96afaadd1f5d11760bee8f15ec1a218c0
josuesanmor <ja.sanchez21@info.uas.edu.mx>
2026-02-01 00:22:01 -0700

    Conexión hecha

--- config.js
+++ config.js
@@ -7,6 +7,9 @@ export const db = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_DATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
 export default {
